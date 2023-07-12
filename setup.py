1061b2093f0ed68043af384c85dc7cb6343a9083
RandyRDavila <davilar@uhd.edu>
2023-07-12 12:30:06 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='TxGraffiti',
+      version='1.0',
+      description='Python library for mathematical conjecturing on graphs',
+      author='Randy Davila',
+      packages=['TxGraffiti'],
+      zip_safe=False)
